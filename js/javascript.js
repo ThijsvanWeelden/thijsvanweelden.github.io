c2b1084446c7e00b811aaed32e0a0624ce4549c0
LAPTOP-IBO1DO9P\thijs <thijsvanweelden11@gmail.com>
2018-01-31 10:02:10 +0100

    no message

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+document.getElementById('knop').onclick = function(){
+	document.getElementsByClassName("personal")[0].style.display = "block";
+	document.getElementsByClassName("personal")[1].style.display = "block";
+
+
+}
